00ca01527a7f12379ac5d86bc3550d7671fe7082
cayla-hhh <cayla.li.huang@gmail.com>
2026-03-30 09:59:19 -0700

    Implement POST and GET for job applications in home route

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('dashboard.html')
+    if request.method == 'POST':
+        conn = sqlite3.connect('job_tracker.db')
+        cursor = conn.cursor()
+        job_title = request.form['job_title']
+        company = request.form['company']
+        deadline = request.form['deadline']
+        status = request.form['status']
+        cursor.execute('INSERT INTO Applications (job_title, company, deadline, status) VALUES (? , ?, ?, ?)', (job_title, company, deadline, status))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('home'))
+    else:
+        conn = sqlite3.connect('job_tracker.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM Applications')
+        apps = cursor.fetchall()
+        conn.close()
+        return render_template('dashboard.html', applications=apps)
+
